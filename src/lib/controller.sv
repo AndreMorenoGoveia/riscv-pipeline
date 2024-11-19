65819014856dd59fb48075e89485281ca75f4b69
AndreMorenoGoveia <andremorgov@gmail.com>
2024-11-18 21:11:00 -0300

    fixes for compile

--- src/lib/controller.sv
+++ src/lib/controller.sv
@@ -7,8 +7,7 @@ module controller(input logic clk, reset,
     // Execute stage control signals
     input logic FlushE,
     input logic ZeroE,
-    output logic PCSrcE, // for datapath and
-    Hazard Unit
+    output logic PCSrcE, // for datapath and Hazard Unit
     output logic [2:0] ALUControlE,
     output logic ALUSrcAE,
     output logic ALUSrcBE, // for lui
@@ -18,8 +17,7 @@ module controller(input logic clk, reset,
     output logic RegWriteM, // for Hazard Unit
 
     // Writeback stage control signals
-    output logic RegWriteW, // for datapath and
-    Hazard Unit
+    output logic RegWriteW, // for datapath and Hazard Unit
     output logic [1:0] ResultSrcW);
     // pipelined control signals
     logic RegWriteD, RegWriteE;
@@ -90,22 +88,25 @@ module aludec(input logic opb5,
     input logic [2:0] funct3,
     input logic funct7b5,
     input logic [1:0] ALUOp,
-    output logic [2:0] ALUControl);
+    output logic [2:0] ALUControl
+    );
+
     logic RtypeSub;
-    assign RtypeSub = funct7b5 & opb5; // TRUE for R-type subtract instruction always_comb
-    case(ALUOp)
-        2'b00: ALUControl = 3'b000; // addition
-        2'b01: ALUControl = 3'b001; // subtraction
-        default: case(funct3) // R-type or I-type ALU
-                    3'b000: if (RtypeSub)
-                                ALUControl = 3'b001; // sub
-                    else
-                        ALUControl = 3'b000; // add, addi
-                    3'b010: ALUControl = 3'b101; // slt, slti
-                    3'b100: ALUControl = 3'b100; // xor
-                    3'b110: ALUControl = 3'b011; // or, ori
-                    3'b111: ALUControl = 3'b010; // and, andi
-                    default: ALUControl = 3'bxxx; // ???
-                endcase
-    endcase
+    assign RtypeSub = funct7b5 & opb5; // TRUE for R-type subtract instruction 
+    always_comb
+        case(ALUOp)
+            2'b00: ALUControl = 3'b000; // addition
+            2'b01: ALUControl = 3'b001; // subtraction
+            default: case(funct3) // R-type or I-type ALU
+                        3'b000: if (RtypeSub)
+                                    ALUControl = 3'b001; // sub
+                        else
+                            ALUControl = 3'b000; // add, addi
+                        3'b010: ALUControl = 3'b101; // slt, slti
+                        3'b100: ALUControl = 3'b100; // xor
+                        3'b110: ALUControl = 3'b011; // or, ori
+                        3'b111: ALUControl = 3'b010; // and, andi
+                        default: ALUControl = 3'bxxx; // ???
+                    endcase
+        endcase
 endmodule
